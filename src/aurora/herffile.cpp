91a7525aeb464653eb065119f5af205a2e295a5c
Sven Hesse <drmccoy@drmccoy.de>
2015-04-13 00:51:41 +0200

    AURORA: Add a HERFFile class

---
+++ src/aurora/herffile.cpp
@@ -0,0 +1,195 @@
+/* xoreos-tools - Tools to help with xoreos development
+ *
+ * xoreos-tools is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos-tools is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos-tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  BioWare's HERF (hashed ERF) file parsing.
+ */
+
+#include "src/common/util.h"
+#include "src/common/file.h"
+#include "src/common/filepath.h"
+#include "src/common/stream.h"
+#include "src/common/encoding.h"
+#include "src/common/hash.h"
+
+#include "src/aurora/herffile.h"
+#include "src/aurora/error.h"
+#include "src/aurora/util.h"
+
+namespace Aurora {
+
+HERFFile::HERFFile(const Common::UString &fileName) : _fileName(fileName),
+	_dictOffset(0xFFFFFFFF), _dictSize(0) {
+
+	load();
+}
+
+HERFFile::~HERFFile() {
+}
+
+void HERFFile::clear() {
+	_resources.clear();
+}
+
+void HERFFile::load() {
+	Common::File herf;
+	open(herf);
+
+	uint32 magic = herf.readUint32LE();
+	if (magic != 0x00F1A5C0)
+		throw Common::Exception("Invalid HERF file (0x%08X)", magic);
+
+	uint32 resCount = herf.readUint32LE();
+
+	_resources.resize(resCount);
+	_iResources.resize(resCount);
+
+	try {
+
+		searchDictionary(herf, resCount);
+		readResList(herf);
+
+	if (herf.err())
+		throw Common::Exception(Common::kReadError);
+
+	} catch (Common::Exception &e) {
+		e.add("Failed reading HERF file");
+		throw;
+	}
+
+}
+
+void HERFFile::searchDictionary(Common::SeekableReadStream &herf, uint32 resCount) {
+	const uint32 dictHash = Common::hashStringDJB2("erf.dict");
+
+	uint32 pos = herf.pos();
+
+	for (uint32 i = 0; i < resCount; i++) {
+		uint32 hash = herf.readUint32LE();
+		if (hash == dictHash) {
+			_dictSize   = herf.readUint32LE();
+			_dictOffset = herf.readUint32LE();
+			break;
+		}
+
+		herf.skip(8);
+	}
+
+	herf.seek(pos);
+}
+
+void HERFFile::readDictionary(Common::SeekableReadStream &herf, std::map<uint32, Common::UString> &dict) {
+	if (_dictOffset == 0xFFFFFFFF)
+		return;
+
+	uint32 pos = herf.pos();
+
+	if (!herf.seek(_dictOffset))
+		return;
+
+	uint32 magic = herf.readUint32LE();
+	if (magic != 0x00F1A5C0)
+		throw Common::Exception("Invalid HERF dictionary (0x%08X)", magic);
+
+	uint32 hashCount = herf.readUint32LE();
+
+	for (uint32 i = 0; i < hashCount; i++) {
+		if ((uint32)herf.pos() >= (_dictOffset + _dictSize))
+			break;
+
+		uint32 hash = herf.readUint32LE();
+		dict[hash] = Common::readStringFixed(herf, Common::kEncodingASCII, 128).toLower();
+	}
+
+	herf.seek(pos);
+}
+
+void HERFFile::readResList(Common::SeekableReadStream &herf) {
+	std::map<uint32, Common::UString> dict;
+	readDictionary(herf, dict);
+
+	uint32 index = 0;
+	ResourceList::iterator   res = _resources.begin();
+	IResourceList::iterator iRes = _iResources.begin();
+	for (; (res != _resources.end()) && (iRes != _iResources.end()); ++index, ++res, ++iRes) {
+		res->index = index;
+
+		res->hash = herf.readUint32LE();
+
+		iRes->size   = herf.readUint32LE();
+		iRes->offset = herf.readUint32LE();
+
+		if (iRes->offset >= (uint32)herf.size())
+			throw Common::Exception("HERFFile::readResList(): Resource goes beyond end of file");
+
+		std::map<uint32, Common::UString>::const_iterator name = dict.find(res->hash);
+		if (name != dict.end()) {
+			res->name = Common::FilePath::getStem(name->second);
+			res->type = getFileType(name->second);
+		}
+	}
+}
+
+const Archive::ResourceList &HERFFile::getResources() const {
+	return _resources;
+}
+
+const HERFFile::IResource &HERFFile::getIResource(uint32 index) const {
+	if (index >= _iResources.size())
+		throw Common::Exception("Resource index out of range (%d/%d)", index, _iResources.size());
+
+	return _iResources[index];
+}
+
+uint32 HERFFile::getResourceSize(uint32 index) const {
+	return getIResource(index).size;
+}
+
+Common::SeekableReadStream *HERFFile::getResource(uint32 index) const {
+	const IResource &res = getIResource(index);
+	if (res.size == 0)
+		return new Common::MemoryReadStream(0, 0);
+
+	Common::File herf;
+	open(herf);
+
+	if (!herf.seek(res.offset))
+		throw Common::Exception(Common::kSeekError);
+
+	Common::SeekableReadStream *resStream = herf.readStream(res.size);
+
+	if (!resStream || (((uint32) resStream->size()) != res.size)) {
+		delete resStream;
+		throw Common::Exception(Common::kReadError);
+	}
+
+	return resStream;
+}
+
+Common::HashAlgo HERFFile::getNameHashAlgo() const {
+	return Common::kHashDJB2;
+}
+
+void HERFFile::open(Common::File &file) const {
+	if (!file.open(_fileName))
+		throw Common::Exception(Common::kOpenError);
+}
+
+} // End of namespace Aurora
---
+++ src/aurora/herffile.h
@@ -0,0 +1,99 @@
+/* xoreos-tools - Tools to help with xoreos development
+ *
+ * xoreos-tools is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos-tools is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos-tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  BioWare's HERF (hashed ERF) file parsing.
+ */
+
+#ifndef AURORA_HERFFILE_H
+#define AURORA_HERFFILE_H
+
+#include <map>
+
+#include "src/common/types.h"
+#include "src/common/ustring.h"
+
+#include "src/aurora/types.h"
+#include "src/aurora/archive.h"
+
+namespace Common {
+	class SeekableReadStream;
+	class File;
+}
+
+namespace Aurora {
+
+/** Class to hold resource data of an HERF file. */
+class HERFFile : public Archive {
+public:
+	HERFFile(const Common::UString &fileName);
+	~HERFFile();
+
+	/** Clear the resource list. */
+	void clear();
+
+	/** Return the list of resources. */
+	const ResourceList &getResources() const;
+
+	/** Return the size of a resource. */
+	uint32 getResourceSize(uint32 index) const;
+
+	/** Return a stream of the resource's contents. */
+	Common::SeekableReadStream *getResource(uint32 index) const;
+
+	/** Return with which algorithm the name is hashed. */
+	Common::HashAlgo getNameHashAlgo() const;
+
+private:
+	/** Internal resource information. */
+	struct IResource {
+		uint32 offset;   ///< The offset of the resource within the HERF.
+		uint32 size;     ///< The resource's size.
+	};
+
+	typedef std::vector<IResource> IResourceList;
+
+	/** External list of resource names and types. */
+	ResourceList _resources;
+
+	/** Internal list of resource offsets and sizes. */
+	IResourceList _iResources;
+
+	/** The name of the HERF file. */
+	Common::UString _fileName;
+
+	uint32 _dictOffset; ///< The offset of the dict file (if available).
+	uint32 _dictSize;   ///< The size of the dict file (if available).
+
+	void open(Common::File &file) const;
+
+	void load();
+	void searchDictionary(Common::SeekableReadStream &herf, uint32 resCount);
+	void readDictionary(Common::SeekableReadStream &herf, std::map<uint32, Common::UString> &dict);
+	void readResList(Common::SeekableReadStream &herf);
+
+	void readNames();
+
+	const IResource &getIResource(uint32 index) const;
+};
+
+} // End of namespace Aurora
+
+#endif // AURORA_NDSROM_H
