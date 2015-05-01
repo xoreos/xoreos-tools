e83049c78cbfa765ae487bccdfb62ab200fea283
Sven Hesse <drmccoy@drmccoy.de>
2015-05-01 14:13:03 +0200

    XML: Add a TLK->XML dumper class

---
+++ src/xml/tlkdumper.cpp
@@ -0,0 +1,83 @@
+/* xoreos-tools - Tools to help with xoreos development
+ *
+ * xoreos-tools is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos-tools is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos-tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Dump TLKs into XML files.
+ */
+
+#include "src/common/stream.h"
+
+#include "src/aurora/talktable.h"
+
+#include "src/xml/tlkdumper.h"
+#include "src/xml/xmlwriter.h"
+
+namespace XML {
+
+TLKDumper::TLKDumper() {
+}
+
+TLKDumper::~TLKDumper() {
+}
+
+void TLKDumper::dump(Common::WriteStream &output, Common::SeekableReadStream &input,
+                     Common::Encoding encoding) {
+
+	Aurora::TalkTable *tlk = Aurora::TalkTable::load(input, encoding);
+	if (!tlk)
+		return;
+
+	XMLWriter xml(output);
+
+	xml.openTag("tlk");
+	xml.breakLine();
+
+	const std::list<uint32> &strRefs = tlk->getStrRefs();
+
+	for (std::list<uint32>::const_iterator s = strRefs.begin(); s != strRefs.end(); ++s) {
+		const uint32 strRef = *s;
+
+		Common::UString str, sound;
+		tlk->getString(strRef, str, sound);
+
+		if (str.empty())
+			continue;
+
+		xml.openTag("string");
+		xml.addProperty("id", Common::UString::sprintf("%u", strRef));
+
+		if (!sound.empty())
+			xml.addProperty("sound", sound);
+
+		xml.setContents(str);
+
+		xml.closeTag();
+		xml.breakLine();
+	}
+
+	xml.closeTag();
+	xml.breakLine();
+
+	xml.flush();
+
+	delete tlk;
+}
+
+} // End of namespace XML
---
+++ src/xml/tlkdumper.h
@@ -0,0 +1,48 @@
+/* xoreos-tools - Tools to help with xoreos development
+ *
+ * xoreos-tools is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos-tools is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos-tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Dump TLKs into XML files.
+ */
+
+#ifndef XML_TLKDUMPER_H
+#define XML_TLKDUMPER_H
+
+#include "src/common/encoding.h"
+
+namespace Common {
+	class SeekableReadStream;
+	class WriteStream;
+}
+
+namespace XML {
+
+class TLKDumper {
+public:
+	TLKDumper();
+	~TLKDumper();
+
+	/** Dump the TLK into XML. */
+	void dump(Common::WriteStream &output, Common::SeekableReadStream &input, Common::Encoding encoding);
+};
+
+} // End of namespace XML
+
+#endif // XML_TLKDUMPER_H
