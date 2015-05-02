9b730b7e0a0aa47c7d9e167f6896c3b1cb536ba7
Sven Hesse <drmccoy@drmccoy.de>
2015-05-02 21:40:37 +0200

    AURORA: Add a decompressor for "small" files
    Nintendo DS LZSS (types 0x00 and 0x10), found in Sonic.
    
    Loosely based on DSDecmp by Barubary, released under the terms of the MIT license.
    
    https://github.com/gravgun/dsdecmp/blob/master/CSharp/DSDecmp/Formats/Nitro/LZ10.cs#L121
    https://code.google.com/p/dsdecmp/

---
+++ src/aurora/smallfile.h
@@ -0,0 +1,44 @@
+/* xoreos-tools - Tools to help with xoreos development
+ *
+ * xoreos-tools is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos-tools is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos-tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Decompressing "small" files, Nintendo DS LZSS (types 0x00 and 0x10), found in Sonic.
+ */
+
+#ifndef AURORA_SMALLFILE_H
+#define AURORA_SMALLFILE_H
+
+namespace Common {
+	class SeekableReadStream;
+	class WriteStream;
+}
+
+namespace Aurora {
+
+class Small {
+public:
+	static void decompress(Common::SeekableReadStream &small, Common::WriteStream &out);
+
+	static Common::SeekableReadStream *decompress(Common::SeekableReadStream *small);
+};
+
+} // End of namespace Aurora
+
+#endif // AURORA_SMALLFILE_H
