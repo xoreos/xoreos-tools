40f32f0c06e8d967f782800ed80b94132ff447c1
Sven Hesse <drmccoy@drmccoy.de>
2015-06-10 21:05:36 +0200

    UNERF: Add command v, to list archive with directory names

--- src/unerf.cpp
+++ src/unerf.cpp
@@ -39,14 +39,15 @@
 #include "src/files_dragonage.h"
 
 enum Command {
-	kCommandNone    = -1,
-	kCommandInfo    =  0,
-	kCommandList        ,
-	kCommandExtract     ,
+	kCommandNone        = -1,
+	kCommandInfo        =  0,
+	kCommandList            ,
+	kCommandListVerbose     ,
+	kCommandExtract         ,
 	kCommandMAX
 };
 
-const char *kCommandChar[kCommandMAX] = { "i", "l", "e" };
+const char *kCommandChar[kCommandMAX] = { "i", "l", "v", "e" };
 
 void printUsage(FILE *stream, const char *name);
 bool parseCommandLine(int argc, char **argv, int &returnValue,
@@ -56,6 +57,7 @@ bool findHashedName(uint64 hash, Common::UString &name);
 
 void displayInfo(Aurora::ERFFile &erf);
 void listFiles(Aurora::ERFFile &rim, Aurora::GameID game);
+void listVerboseFiles(Aurora::ERFFile &rim, Aurora::GameID game);
 void extractFiles(Aurora::ERFFile &rim, Aurora::GameID);
 
 int main(int argc, char **argv) {
@@ -74,6 +76,8 @@ int main(int argc, char **argv) {
 			displayInfo(erf);
 		else if (command == kCommandList)
 			listFiles(erf, game);
+		else if (command == kCommandListVerbose)
+			listVerboseFiles(erf, game);
 		else if (command == kCommandExtract)
 			extractFiles(erf, game);
 
@@ -153,13 +157,14 @@ void printUsage(FILE *stream, const char *name) {
 	std::fprintf(stream, "Commands:\n");
 	std::fprintf(stream, "  i          Display meta-information\n");
 	std::fprintf(stream, "  l          List archive\n");
+	std::fprintf(stream, "  v          List archive verbosely (show directory names)\n");
 	std::fprintf(stream, "  e          Extract files to current directory\n");
 }
 
 bool findHashedName(uint64 hash, Common::UString &name) {
 	const char *fileName = findDragonAgeFile(hash);
 	if (fileName) {
-		name = Common::FilePath::getStem(fileName);
+		name = Common::FilePath::changeExtension(fileName, "");
 		return true;
 	}
 
@@ -206,9 +211,55 @@ void listFiles(Aurora::ERFFile &erf, Aurora::GameID game) {
 		if (name.empty())
 			findHashedName(r->hash, name);
 
+		name = Common::FilePath::getFile(name);
+
 		std::printf("%32s%-4s | %10d\n", name.c_str(), TypeMan.setFileType("", type).c_str(),
 		                                 erf.getResourceSize(r->index));
 	}
+}
+
+struct FileEntry {
+	Common::UString file;
+	uint32 size;
+
+	FileEntry(const Common::UString &f = "", uint32 s = -1) : file(f), size(s) { }
+};
+
+void listVerboseFiles(Aurora::ERFFile &erf, Aurora::GameID game) {
+	const Aurora::Archive::ResourceList &resources = erf.getResources();
+	const uint32 fileCount = resources.size();
+
+	std::printf("Number of files: %u\n\n", fileCount);
+
+	std::vector<FileEntry> fileEntries;
+	fileEntries.reserve(fileCount);
+
+	uint32 nameLength = 10;
+	for (Aurora::Archive::ResourceList::const_iterator r = resources.begin(); r != resources.end(); ++r) {
+		const Aurora::FileType type = TypeMan.aliasFileType(r->type, game);
+
+		Common::UString name = r->name;
+		if (name.empty())
+			findHashedName(r->hash, name);
+
+		name.replaceAll('\\', '/');
+
+		name = TypeMan.addFileType(name, type);
+
+		nameLength = MAX<uint32>(nameLength, name.size() + 1);
+
+		fileEntries.push_back(FileEntry(name, erf.getResourceSize(r->index)));
+	}
+
+	if ((nameLength % 2) == 1)
+		nameLength++;
+
+	std::printf("%sFileName%s|    Size\n", Common::UString(' ', (nameLength - 8) / 2).c_str(),
+	                                       Common::UString(' ', (nameLength - 8) / 2).c_str());
+	std::printf("%s|===========\n", Common::UString('=', nameLength).c_str());
+
+	for (std::vector<FileEntry>::const_iterator f = fileEntries.begin(); f != fileEntries.end(); ++f)
+		std::printf("%-*s| %10d\n", nameLength, f->file.c_str(), f->size);
 }
 
 void extractFiles(Aurora::ERFFile &erf, Aurora::GameID game) {
