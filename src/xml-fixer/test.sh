5d396515778641750d9ed0c90b3f3836f577718c
asr1 <alexandersrinehart@gmail.com>
2015-06-20 10:46:51 -0700

    added test.sh, which does an automated test of every file, letting us know which passed and failed.

---
+++ src/xml-fixer/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+for f in *Fixed.xml ; do rm $f ; done #Delete old files so we don't run this on them multiple times.
+
+for f in *.xml ; do
+	./XMLFix $f > /dev/null  2> /dev/null #I don't want to see much output.
+done
+
+pass=0
+fail=0
+
+#Clear our pass/fail logs
+echo "" > pass.txt
+echo "" > fail.txt 
+
+for f in *Fixed.xml ; do 
+	result=$(xmllint $f |& tail -n 1)
+	if [ $result = "</Root>" ] ; then
+		echo "$f Passed" >> pass.txt
+		pass=$(( $pass + 1 ))
+	else
+		echo "$f Failed" >> fail.txt
+		fail=$(( $fail + 1 ))
+	fi
+done
+
+echo "Pass = $pass"
+echo "Fail = $fail"
