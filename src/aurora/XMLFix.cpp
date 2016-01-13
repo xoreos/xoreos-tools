6f3c4a4b4e8cc68849f644e4c733c6ff0334d0ea
asr1 <alexandersrinehart@gmail.com>
2016-01-13 13:02:05 -0600

    Clean up code

--- src/aurora/XMLFix.cpp
+++ src/aurora/XMLFix.cpp
@@ -1,3 +1,8 @@
+//Converts NWN xml code to proper XML code.
+//Fixes unescaped special characters, root
+//Elements, mismatched nodes, unclosed 
+//Parentheses, and unclosed quotes.
+
 
 #include <iostream>
 #include <string>
@@ -5,6 +10,7 @@
 #include <algorithm>
 #include <stdio.h>
 #include <ctype.h>
+#include "../common/ustring.h"
 #include "XMLFix.h"
 
 using std::cout;
@@ -12,19 +18,19 @@ using std::string;
 using namespace std; //I'll fix this later
 
 int comCount = 0; //Track number of open/closed comments.
-bool inUIButton = false;
+bool inUIButton = false; //Used to fix </UIButton> tags that are never opened. 
 
 int main(int argc, char* argv[]){
 	if (argc != 2){
 		cout<<"Please specify an xml file to parse.\n"
-		"a file name <fileName>Fixed will be created.\n";
+		"a file name <fileName>Fixed will be created.\n";//TODO: update this when we no longer create new files. Double-check, but I believe we want to modify the original file, not create a new one. But double check before we change THAT particular part of the implementation.
 		return -1;
 	}	
 	//First get our file names	
-	std::string oldFileName = argv[1];
-	std::string newFileName = oldFileName;
-	std::string line; //Each line of XML we parse
-	int perLoc = oldFileName.find("."); //Location of period in the file name.
+	Common::UString oldFileName = argv[1];
+	Common::UString newFileName = oldFileName;
+	Common::UString line; //Each line of XML we parse
+	size_t perLoc = oldFileName.find("."); //Location of period in the file name.
 	//There is a period	
 	if (perLoc != std::string::npos){//Add Fixed before the extension
 		newFileName.insert(perLoc, "Fixed");
@@ -56,7 +62,7 @@ int main(int argc, char* argv[]){
 		cout << "Error reading file\n";
 		return -1;
 	}
-	//Then put the root element.
+	//Then insert the root element.
 	writeFile << "<Root>\n";
 	while (getline (readFile, line)){
 		countComments(line);
@@ -68,21 +74,22 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
-//Read and fix any line of XML that is passed in,
-//Returns that fixed line.
-std::string parseLine(std::string line){
+/**Read and fix any line of XML that is passed in,
+ * Returns that fixed line.
+ */
+Common::UString parseLine(Common::UString line){
 	line = fixUnclosedNodes(line);
-	line = escapeSpacedStrings(line, false);
+	line = escapeSpacedStrings(line, false);//Fix problematic strings (with spaces or special characters)
 	line = fixMismatchedParen(line);	
 	line = fixOpenQuotes(line);//It's imperative that this run before
 	//the copyright line, or not on it at all. Could update to ignore
 	//Comments.
 	line = escapeInnerQuotes(line);
-	line = fixCopyright(line); //Does this need to run EVERY line?
+	line = fixCopyright(line); //Does this need to run EVERY line? //Worst case improvement, we could have a global variable for whether or not we've found it, and just run it once per file, on the assumption that it will only appear once per file.
 	line = doubleDashFix(line);
 	line = quotedCloseFix(line);
 	line = tripleQuoteFix(line);
-	line = escapeSpacedStrings(line, true);
+	line = escapeSpacedStrings(line, true);//Restore the problematic strings to their proper values.
 	return line;
 }
 
@@ -93,7 +100,7 @@ std::string parseLine(std::string line){
  * This probably doesn't need to run on every
  * Line.
  */
-std::string fixCopyright(std::string line){
+Common::UString fixCopyright(Common::UString line){
 	//If this is the copyright line, remove the unicode.
 	if (line.find("Copyright") != std::string::npos){
 		if (!comCount){
@@ -105,8 +112,15 @@ std::string fixCopyright(std::string line){
 	return line;
 }
 
-//Corrects improper opening XML tags
-std::string fixXMLTag(std::string line){
+/**Corrects improper opening XML tags.
+ * An improper XML tag has <xml instead
+ * Of <?xml. 
+ * Also changes references to NWN2UI to 
+ * XML so xml-lint reads it properly.
+ * Returns the unmodified line with the
+ * Proper opening XML tag.
+ */
+Common::UString fixXMLTag(Common::UString line){
 	//Let's ensure we close this properly.
 	if (line.find("<?xml") != string::npos){
 		line = trim(line);
@@ -124,10 +138,10 @@ std::string fixXMLTag(std::string line){
  * If there is a close node without an open node
  * This will delete it. Right now it only works 
  * If there is a closed UIButton without an open
- * UIButton
+ * UIButton.
  */
-std::string fixUnclosedNodes(std::string line){
-	int pos = line.find("<UIButton");
+Common::UString fixUnclosedNodes(Common::UString line){
+	size_t pos = line.find("<UIButton");
 	//Open node	
 	if (pos != string::npos){
 		inUIButton = true;
@@ -141,8 +155,6 @@ std::string fixUnclosedNodes(std::string line){
 		}
 		inUIButton = false;
 	}
-
-
 	return line;
 }
 
@@ -150,16 +162,16 @@ std::string fixUnclosedNodes(std::string line){
 /**
  * Finds and escapes quotes in an element,
  * Returns a fixed line.
- * The only time we're seeing "false" quotes is
+ * The only time we're seeing faulty quotes is
  * In the context open("FooBar"), so that's the only
  * Case we look for right now.
  */
-std::string escapeInnerQuotes(std::string line){
+Common::UString escapeInnerQuotes(Common::UString line){
 	if (countOccurances(line, '"') > 2){//We have more than 2 quotes in one line
-		int firstQuotPos =line.find("\""); //The first quotation mark
-		int lastQuotPos = line.find_last_of("\""); //The last quotation mark
+		size_t firstQuotPos =line.find("\""); //The first quotation mark
+		size_t lastQuotPos = line.find_last_of("\""); //The last quotation mark
 		bool inPar = false;
-		for (int i = firstQuotPos + 1; i < lastQuotPos-1; i++){
+		for (size_t i = firstQuotPos + 1; i < lastQuotPos-1; i++){
 			//We're in a parenthetical, all quotes need to be replaced
 			//This is not covered by our previous cases if there are 
 			//Multiple quoted entries in one set of parens.
@@ -186,13 +198,13 @@ std::string escapeInnerQuotes(std::string line){
 }
 
 /**
- * counts the number of times a character, find,
+ * Counts the number of times a character, find,
  * Appears in a string, line, and returns that 
- * Number.
+ * Number. //TODO: can we replace this with std::count?
  */
-int countOccurances(string line, char find){
+int countOccurances(Common::UString, char find){
 	int count = 0;
-	for (int i = 0; i < line.length(); i++){	
+	for (size_t i = 0; i < line.length(); i++){	
 		if (line[i] == find){
 			count++;
 		}
@@ -201,16 +213,16 @@ int countOccurances(string line, char find){
 }
 
 //Adds a closing paren if a line is missing such a thing.
-std::string fixMismatchedParen(std::string line){
+Common::UString fixMismatchedParen(Common::UString line){
 	bool inParen = false;
-	int end = line.length();
-	for (int i = 0; i < end; i++){
+	size_t end = line.length();
+	for (size_t i = 0; i < end; i++){
 		if (!inParen){
 			if (line[i] == '('){
 				inParen = true;
 			}
 		}else{
-			int pos = line.find("/>");
+			size_t pos = line.find("/>");
 				if (line[i] == ')'){
 					inParen = false;
 				}else if (i == pos -1){//We're at the end of the string and haven't closed a paren.
@@ -229,48 +241,47 @@ return line;
  * By a quotation mark. Insert that quotation mark,
  * and return the fixed line.
  */
-std::string fixOpenQuotes(std::string line){
+Common::UString fixOpenQuotes(Common::UString line){
 //We have an equal with no open quote
-	int end = line.length() -1;
-	for (int i = 0; i < end; i++)
-	{
-		if (line.at(i) == '='&& line.at(i+1) != '"')
-		{//Equal sign should be followed by a quote
+	size_t end = line.length() -1;
+	for (size_t i = 0; i < end; i++){
+                //Equal sign should be followed by a quote
+		if (line.at(i) == '='&& line.at(i+1) != '"'){
 			line.insert(i+1,"\"");
 			i++;//Our string got longer.
 			end++;
 		}
 
-		if (line.at(i) == '(' && line.at(i+1) != '"' && line.at(i+1) != ')')
-		{//Open paren should be followed by a &quot; (or an immediate close paren)
+                //Open paren should be followed by a &quot; (or an immediate close paren)
 		//But if we replace it directly here, it will be doubly escaped
 		//Because we run escapeInnerQuotes() next.
+		if (line.at(i) == '(' && line.at(i+1) != '"' && line.at(i+1) != ')'){
 			line.insert(i+1, "\"");
 			end++;
 		}
 
-		if (i > 0 && line.at(i) == ')' && line.at(i-1) != '"' && line.at(i-1) != '(')
-		{//A closed quote should be preceeded by &quot; See above.
+                //A closed quote should be preceeded by &quot; See above.
 		//There are some exceptions to this, like when we have one quoted element
 		//In a 2 element parenthesis set. This is always a number. ("elem="foo",local=5)
 		//Or when we have () empty.
-
+		if (i > 0 && line.at(i) == ')' && line.at(i-1) != '"' && line.at(i-1) != '('){
 			line.insert(i,"\"");
 			end++;
 		}
-		if (i > 0 && line.at(i) == ',' && line.at(i-1) != '"')
-		{//No quote before , add it in.
+                
+                //No quote before , add it in.
+		if (i > 0 && line.at(i) == ',' && line.at(i-1) != '"'){
 			line.insert(i, "\"");
 			end++;
 		}
-		if (line.at(i) == ',' && line.at(i+1) != '"')
-		{//No quote after a comma
+                //No quote after a comma
+		if (line.at(i) == ',' && line.at(i+1) != '"'){
 			line.insert(i+1, "\"");
 			end++;
-		}
 
-		if (i < end -1 && line.at(i) == ')'&& line.at(i+2) != '\\')
-		{//A close paren should be followed by a " or a space and a \>
+		}
+                //A close paren should be followed by a " or a space and a \>
+		if (i < end -1 && line.at(i) == ')'&& line.at(i+2) != '\\'){
 			line.insert(i+1,"\"");
 			i++;//Our string got longer.
 			end++;
@@ -289,26 +300,29 @@ std::string fixOpenQuotes(std::string line){
  * there isn't a close quote, AND we have an
  * odd number of quotes.
  */
-std::string fixUnevenQuotes(std::string line){
-	int closeBrace = line.find("/>");
-	bool temp = closeBrace != string::npos;
+Common::UString fixUnevenQuotes(Common::UString line){
+	size_t closeBrace = line.find("/>");
+        //We don't have a close quote before our close brace
 	//Sometimes there is a space after a quote
 	if (closeBrace != string::npos && closeBrace > 0 &&
 					(line.at(closeBrace-1) != '\"' || line.at(closeBrace-2) != '\"') &&
-					countOccurances(line, '"') % 2){//We don't have a close quote before our close brace
+					countOccurances(line, '"') % 2){
 		line.insert(closeBrace,"\"");
 	}
-return line;
+        return line;
 }
 
 /**
  * After all of this, if we can iterate through a string
  * And find a quote followed by a whitespace character, insert a quote.
+ * Preconditions are such that this should never occur naturally at this
+ * Point in the code, and if we do end up adding too many, it will be
+ * Removed in a later function (such as fixTripleQuote())
  */
 std::string fixUnclosedQuote(std::string line){
-	bool inQuote = false;
-	int end = line.length();
-	for (int i = 0; i < end; i++){
+	bool inQuote = false; //Tracks if we are inside a quote
+	size_t end = line.length();
+	for (size_t i = 0; i < end; i++){
 		if (!inQuote){
 			if (line[i] == '"'){
 				inQuote = true;
@@ -340,14 +354,14 @@ return line;
  */
 std::string fixCloseBraceQuote(std::string line){
 	bool inQuote = false;
-	int end = line.length();
-	for (int i = 0; i < end; i++){
+	size_t end = line.length();
+	for (size_t i = 0; i < end; i++){
 		if (!inQuote){
 			if (line[i] == '"'){
 				inQuote = true;
 			}
 		}else{
-			int pos = line.find("/>");
+			size_t pos = line.find("/>");
 			if (line[i] == '"'){//Inquote is true, we're in a quoted part.
 				inQuote = false;
 			}else if (pos != std::string::npos){
@@ -364,11 +378,13 @@ return line;
 /** 
  * If there are any -- inside of a comment,
  * This will remove them and replace it with
- * A single dash.
+ * A single dash. Otherwise this breaks 
+ * Compatibility. 
  */
 std::string doubleDashFix(std::string line){
-	int pos = line.find("--");
-	if (pos < line.length()-1 && line.at(pos + 2) != '>' && (pos > 0 && line.at(pos-1) != '!')){ //It's not a comment
+	size_t pos = line.find("--");
+        //It's not a comment
+	if (pos < line.length()-1 && line.at(pos + 2) != '>' && (pos > 0 && line.at(pos-1) != '!')){ 
 		line.erase(pos, 1);//Remove one dash.
 	}
 	return line;
@@ -380,12 +396,18 @@ std::string doubleDashFix(std::string line){
  * Let's be honest, this can only happen as a 
  * Result of other methods, and this is a kludgy fix.
  * Note that this will only find one per line.
+ * This will also remove double quotes that are not
+ * Intended to be in the XML. name="" is the only
+ * Legitimate appearance of "" in the NWN xml code.
+ * Returns the modified line, without double or 
+ * Triple quotes.
  */
 std::string tripleQuoteFix(std::string line){
-	int pos = line.find("\"\"\"");
+	size_t pos = line.find("\"\"\"");
 	if (pos != std::string::npos){
 		line.erase(pos, 2);//Remove two quotes.
 	}
+
 	//Might as well escape "" as well, while we're at it.
 	if (line.find("name=\"\"") == std::string::npos){//If the line doesn't contain name=""	
 		pos = line.find("\"\"");
@@ -397,41 +419,45 @@ std::string tripleQuoteFix(std::string line){
 }
 
 /**
- * Some values are correct, but need to be
- * Flagged so other algorithms don't pick
- * Up on them.
- * Replaces these values with "escaped" safe strings, 
- * Then undoes it the second time it's called.
- * The issue here is primarily when strings with spaces are assigned as values.
+ * Some lines contain problematic phrases. (phrases that include
+ * String literals with spaces or the > or / characters).
+ * If left untouched, other functions will destroy these strings
+ * Instead of fixing them.
+ * Returns a safe string (devoid of problematic phrases) if undo is false, or
+ * The original string, with problematic phrases restored if undo is true.
  */
 std::string escapeSpacedStrings(std::string line, bool undo){
-	//Just used as containers. There's no other way around the > character breaking this thing.
+	//Just used as containers.
+        //Might be an easier/cleaner way to do this, perhaps with some sort of map instead of two arrays.
 	string switchWordsFrom[] = {"portrait frame" , "0 / 0 MB", "->", ">>", 
 				"capturemouseevents=false", "Speaker Name", " = ", "Player Chat"};
 	string switchWordsTo[] = {"portrait_frame" , "0_/_0_MB", "ReplaceMe1", 
 				"ReplaceMe2","capturemouseevents=false ", "Speaker_Name", "=", "Player_Chat"};
 	
-	//The ones we actually references
+	//The arrays we actually reference
 	string * fromTemp = switchWordsFrom;
 	string * toTemp = switchWordsTo;	
 	//Swap
-	if (undo){//The second time we call this, we want to switch it back.
-		//only c++ 2011 has support for native array swap
+        //No need to switch the first time, but The second time we
+        //Call this, we want to switch from safe to original strings
+	if (undo){
+		//Native array swap wasn't introduced until c++ 2011
 		//So we do this with pointers.	
 		string * swapTemp = fromTemp;		
 		fromTemp = toTemp;
 		toTemp = swapTemp;
 	}
+        //Number of elements in the array.
 	int length = sizeof(switchWordsFrom)/sizeof(switchWordsFrom[0]);
-	for (int i = 0; i < length; i++){
-		int pos = line.find(*(fromTemp+i));
+        //Do the actual replacement inline.	
+        for (int i = 0; i < length; i++){
+		size_t pos = line.find(*(fromTemp+i));
 		if (pos != std::string::npos){
 			line.replace(pos, (fromTemp + i)->length(), *(toTemp + i));
 		}
 	}
 	return line;
 }
-
 
 
 /**
@@ -450,10 +476,16 @@ void countComments(std::string line){
 }
 
 
-//Remove leading and trailing whitespace
+
+//TODO replace this with Common::UString.trim()
+/**
+ * Remove leading and trailing whitespace.
+ * Returns line without leading and trailing 
+ * Spaces.
+ */
 std::string trim(std::string line){
 	string whitespace = " \t\n\v\f\r";
-	int lineBegin = line.find_first_not_of(whitespace);
+	size_t lineBegin = line.find_first_not_of(whitespace);
 	if (lineBegin == std::string::npos){
 	        return ""; //empty string
 	}
@@ -467,10 +499,10 @@ std::string trim(std::string line){
  * If we have a />", raplce it with a />
  * If we have a >", replace it with a ">
  * This function is another instance of 
- * Cleaning up after ourselves
+ * Cleaning up after ourselves.
  */
 std::string quotedCloseFix(std:: string line){
-	int pos = line.find("\"/>\"");
+	size_t pos = line.find("\"/>\"");
 	if (pos != std::string::npos){
 		line.erase(pos, 1);
 		line.erase(pos + 2,1);
