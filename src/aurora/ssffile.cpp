e443f7b7103d879ddf80ce8e4c8b68453774b8a8
Sven Hesse <drmccoy@drmccoy.de>
2016-04-13 11:50:52 +0200

    AURORA: Add class SSFFile
    Copied from xoreos.

---
+++ src/aurora/ssffile.cpp
@@ -0,0 +1,160 @@
+/* xoreos-tools - Tools to help with xoreos development
+ *
+ * xoreos-tools is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos-tools is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos-tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Handling BioWare's SSFs (sound set file).
+ */
+
+/* See BioWare's own specs released for Neverwinter Nights modding
+ * (<https://github.com/xoreos/xoreos-docs/tree/master/specs/bioware>)
+ */
+
+#include "src/common/readstream.h"
+#include "src/common/util.h"
+#include "src/common/strutil.h"
+#include "src/common/error.h"
+#include "src/common/encoding.h"
+
+#include "src/aurora/ssffile.h"
+
+static const uint32 kSSFID     = MKTAG('S', 'S', 'F', ' ');
+static const uint32 kVersion10 = MKTAG('V', '1', '.', '0');
+static const uint32 kVersion11 = MKTAG('V', '1', '.', '1');
+
+namespace Aurora {
+
+SSFFile::SSFFile(Common::SeekableReadStream &ssf) {
+	load(ssf);
+
+	_emptySound.strRef = kStrRefInvalid;
+}
+
+SSFFile::~SSFFile() {
+}
+
+void SSFFile::load(Common::SeekableReadStream &ssf) {
+	try {
+		size_t entryCount, offEntryTable;
+		Version version = readSSFHeader(ssf, entryCount, offEntryTable);
+
+		_sounds.resize(entryCount);
+
+		readEntries(ssf, version, offEntryTable);
+
+	} catch (Common::Exception &e) {
+		e.add("Failed reading SSF file");
+		throw;
+	}
+}
+
+SSFFile::Version SSFFile::readSSFHeader(Common::SeekableReadStream &ssf,
+                                        size_t &entryCount, size_t &offEntryTable) {
+	readHeader(ssf);
+
+	if (_id != kSSFID)
+		throw Common::Exception("Not a SSF file (%s)", Common::debugTag(_id).c_str());
+
+	if ((_version != kVersion10) && (_version != kVersion11))
+		throw Common::Exception("Unsupported SSF file version %s", Common::debugTag(_version).c_str());
+
+	entryCount    = ssf.readUint32LE();
+	offEntryTable = ssf.readUint32LE();
+
+	// Plain old version V1.0 used in NWN (and NWN2)
+	if (_version == kVersion10)
+		return kVersion10_NWN;
+
+	// NWN2's V1.1
+	if ((offEntryTable < ssf.size()) && ((ssf.size() - offEntryTable) >= ((4 + 32 + 4) * entryCount)))
+		return kVersion11_NWN2;
+
+	offEntryTable = entryCount;
+	entryCount    = (ssf.size() - offEntryTable) / 4;
+
+	// Sanity check
+	if ((offEntryTable > ssf.size()) || (((ssf.size() - offEntryTable) % 4) != 0))
+		throw Common::Exception("Invalid SSF header (%u, %u)", (uint32) ssf.size(), (uint32) offEntryTable);
+
+	// KotOR's V1.1
+	return kVersion11_KotOR;
+}
+
+void SSFFile::readEntries(Common::SeekableReadStream &ssf, Version version, size_t offset) {
+	ssf.seek(offset);
+
+	switch (version) {
+		case kVersion10_NWN:
+			readEntriesNWN(ssf, 16);
+			break;
+
+		case kVersion11_NWN2:
+			readEntriesNWN(ssf, 32);
+			break;
+
+		case kVersion11_KotOR:
+			readEntriesKotOR(ssf);
+			break;
+
+		default:
+			break;
+	}
+}
+
+void SSFFile::readEntriesNWN(Common::SeekableReadStream &ssf, size_t soundFileLen) {
+	/* The NWN/NWN2 versions of an SSF file (V1.0 and V1.1) begin with a list of
+	   offsets to the data entries. Each of these contains a ResRef of a sound
+	   file and a StrRef of a text. */
+
+	size_t count = _sounds.size();
+
+	std::vector<uint32> offsets;
+
+	offsets.resize(count);
+
+	for (size_t i = 0; i < count; i++)
+		offsets[i] = ssf.readUint32LE();
+
+	for (size_t i = 0; i < count; i++) {
+		ssf.seek(offsets[i]);
+
+		_sounds[i].fileName = Common::readStringFixed(ssf, Common::kEncodingASCII, soundFileLen);
+		_sounds[i].strRef   = ssf.readUint32LE();
+	}
+}
+
+void SSFFile::readEntriesKotOR(Common::SeekableReadStream &ssf) {
+	/* The KotOR/KotOR2 version of an SSF file (V1.1) is just a list of StrRefs. */
+
+	for (SoundSet::iterator sound = _sounds.begin(); sound != _sounds.end(); ++sound)
+		sound->strRef = ssf.readUint32LE();
+}
+
+size_t SSFFile::getSoundCount() const {
+	return _sounds.size();
+}
+
+const SSFFile::Sound &SSFFile::getSound(size_t index) const {
+	if (index >= _sounds.size())
+		return _emptySound;
+
+	return _sounds[index];
+}
+
+} // End of namespace Aurora
---
+++ src/aurora/ssffile.h
@@ -0,0 +1,97 @@
+/* xoreos-tools - Tools to help with xoreos development
+ *
+ * xoreos-tools is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos-tools is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos-tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Handling BioWare's SSFs (sound set file).
+ */
+
+#ifndef AURORA_SSFFILE_H
+#define AURORA_SSFFILE_H
+
+#include <vector>
+
+#include "src/common/types.h"
+#include "src/common/ustring.h"
+
+#include "src/aurora/types.h"
+#include "src/aurora/aurorafile.h"
+
+namespace Common {
+	class SeekableReadStream;
+}
+
+namespace Aurora {
+
+/** Class to hold a sound set.
+ *
+ *  A sound set contains a simple array of strings and sound resource
+ *  names for common creature sounds, like fighting noises, being
+ *  hurt, calling for medical help, etc.
+ *
+ *  Which index is used for which situation differs from game to game.
+ *
+ *  SSF files are used in these four games: Neverwinter Nights,
+ *  Neverwinter Nights 2, Knights of the Old Republic and Knights
+ *  of the Old Republic II.
+ */
+class SSFFile : public AuroraFile {
+public:
+	/** A sound in the sound set. */
+	struct Sound {
+		Common::UString fileName; ///< The name of the sound file to play.
+		uint32          strRef;   ///< StrRef of the text to display.
+	};
+
+	SSFFile(Common::SeekableReadStream &ssf);
+	~SSFFile();
+
+	/** Return the number of sounds in this SSF file. */
+	size_t getSoundCount() const;
+
+	const Sound &getSound(size_t index) const;
+
+private:
+	enum Version {
+		kVersion10_NWN,
+		kVersion11_NWN2,
+		kVersion11_KotOR
+	};
+
+	typedef std::vector<Sound> SoundSet;
+
+	Sound _emptySound;
+	SoundSet _sounds;
+
+	void load(Common::SeekableReadStream &ssf);
+
+	/** Read the header of an SSF file and determine the version. */
+	Version readSSFHeader(Common::SeekableReadStream &ssf, size_t &entryCount, size_t &offEntryTable);
+
+	/** Read the data entries, depending on the version. */
+	void readEntries(Common::SeekableReadStream &ssf, Version version, size_t offset);
+	/** Read the data entries of the NWN versions. */
+	void readEntriesNWN(Common::SeekableReadStream &ssf, size_t soundFileLen);
+	/** Read the data entries of the KotOR version. */
+	void readEntriesKotOR(Common::SeekableReadStream &ssf);
+};
+
+} // End of namespace Aurora
+
+#endif // AURORA_SSFFILE_H
