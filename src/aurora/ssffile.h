8b2ffad2252e8eed0b26830050f46a8e4817f2b3
Sven Hesse <drmccoy@drmccoy.de>
2016-04-19 18:15:40 +0200

    AURORA: Add SSFFile::determineVersionForGame()
    Determine the best version to save this SSF file in.

--- src/aurora/ssffile.h
+++ src/aurora/ssffile.h
@@ -84,6 +84,11 @@ public:
 	void setSound(size_t index, const Common::UString &soundFile, uint32 strRef);
 	// '---
 
+	// .--- Writing SSF files
+	/** Determine the best version to save this SSF file in, for the specified game. */
+	Version determineVersionForGame(GameID game) const;
+	// '---
+
 private:
 	/** A sound in the sound set. */
 	struct Sound {
@@ -108,6 +113,9 @@ private:
 	void readEntriesNWN(Common::SeekableReadStream &ssf, size_t soundFileLen);
 	/** Read the data entries of the KotOR version. */
 	void readEntriesKotOR(Common::SeekableReadStream &ssf);
+
+	/** Return the maximum length of a sound filename in characters. */
+	size_t getMaxSoundFileLen() const;
 };
 
 } // End of namespace Aurora
