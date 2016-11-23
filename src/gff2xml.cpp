ed5e7b120d42e5e8b669309f631dcfb9ba3e2c93
Matthias Gatto <uso.cosmo.ray@gmail.com>
2016-11-23 14:37:26 +0100

    GFF2XML: Use CLI class

--- src/gff2xml.cpp
+++ src/gff2xml.cpp
@@ -37,6 +37,7 @@
 #include "src/common/writefile.h"
 #include "src/common/stdoutstream.h"
 #include "src/common/encoding.h"
+#include "src/common/cli.h"
 
 #include "src/aurora/types.h"
 #include "src/aurora/language.h"
@@ -47,7 +48,6 @@
 
 typedef std::map<uint32, Common::Encoding> EncodingOverrides;
 
-void printUsage(FILE *stream, const Common::UString &name);
 bool parseCommandLine(const std::vector<Common::UString> &argv, int &returnValue,
                       Common::UString &inFile, Common::UString &outFile,
                       Common::Encoding &encoding, Aurora::GameID &game,
@@ -119,145 +119,62 @@ bool parseCommandLine(const std::vector<Common::UString> &argv, int &returnValue
                       Common::UString &inFile, Common::UString &outFile,
                       Common::Encoding &encoding, Aurora::GameID &game,
                       EncodingOverrides &encOverrides, bool &nwnPremium) {
+	using Common::CLI::NoOption;
+	using Common::CLI::kContinueParsing;
+	using Common::CLI::Parser;
+	using Common::CLI::ValGetter;
+	using Common::CLI::Callback;
+	using Common::CLI::ValAssigner;
+	using Common::CLI::makeEndArgs;
+	using Common::CLI::makeAssigners;
+	using Aurora::GameID;
 
-	inFile.clear();
-	outFile.clear();
-	std::vector<Common::UString> args;
+	NoOption inFileOpt(false, new ValGetter<Common::UString &>(inFile, "input files"));
+	NoOption outFileOpt(true, new ValGetter<Common::UString &>(outFile, "output files"));
+	Parser parser(argv[0], "BioWare GFF to XML converter",
+		      "If no output file is given, the output is written to stdout.\n\n"
+		      "Depending on the game, LocStrings in GFF files might be encoded in various\n"
+		      "ways and there's no way to autodetect how. If a game is specified, the\n"
+		      "encoding tables for this game are used. Otherwise, gff2xml tries some\n"
+		      "heuristics that might fail for certain strings.\n\n"
+		      "Additionally, the --encoding parameter can be used to override the encoding\n"
+		      "for a specific language ID. The string has to be of the form n=encoding,\n"
+		      "for example 0=cp-1252 to override the encoding of the (ungendered) language\n"
+		      "ID 0 to be Windows codepage 1252. To override several encodings, specify\n"
+		      "the --encoding parameter multiple times.\n",
+		      returnValue,
+		      makeEndArgs(&inFileOpt, &outFileOpt));
 
-	bool optionsEnd = false;
-	for (size_t i = 1; i < argv.size(); i++) {
-		bool isOption = false;
 
-		// A "--" marks an end to all options
-		if (argv[i] == "--") {
-			optionsEnd = true;
-			continue;
-		}
+	parser.addOption("cp1252", "Read GFF4 strings as Windows CP-1252", kContinueParsing,
+			 makeAssigners(new ValAssigner<Common::Encoding>(Common::kEncodingCP1252,
+									 encoding)));
+	parser.addOption("nwnpremium", "This is a broken GFF from a Neverwinter Nights premium module",
+			 kContinueParsing,
+			 makeAssigners(new ValAssigner<bool>(true, nwnPremium)));
+	parser.addOption("nwn", "Use Neverwinter Nights encodings", kContinueParsing,
+			 makeAssigners(new ValAssigner<GameID>(Aurora::kGameIDNWN, game)));
+	parser.addOption("nwn2", "Use Neverwinter Nights 2 encodings", kContinueParsing,
+			 makeAssigners(new ValAssigner<GameID>(Aurora::kGameIDNWN2, game)));
+	parser.addOption("kotor", "Use Knights of the Old Republic encodings", kContinueParsing,
+			 makeAssigners(new ValAssigner<GameID>(Aurora::kGameIDKotOR, game)));
+	parser.addOption("kotor2", "Use Knights of the Old Republic II encodings", kContinueParsing,
+			 makeAssigners(new ValAssigner<GameID>(Aurora::kGameIDKotOR2, game)));
+	parser.addOption("jade", "Use Jade Empire encodings", kContinueParsing,
+			 makeAssigners(new ValAssigner<GameID>(Aurora::kGameIDJade, game)));
+	parser.addOption("witcher", "Use The Witcher encodings", kContinueParsing,
+			 makeAssigners(new ValAssigner<GameID>(Aurora::kGameIDWitcher, game)));
+	parser.addOption("dragonage", "Use Dragon Age encodings", kContinueParsing,
+			 makeAssigners(new ValAssigner<GameID>(Aurora::kGameIDDragonAge, game)));
+	parser.addOption("dragonage2", "Use Dragon Age II encodings", kContinueParsing,
+			 makeAssigners(new ValAssigner<GameID>(Aurora::kGameIDDragonAge2, game)));
+	parser.addSpace();
+	parser.addOption("encoding", "Override an encoding", kContinueParsing,
+			 new Callback<EncodingOverrides &>("str", parseEncodingOverride, encOverrides));
 
-		// We're still handling options
-		if (!optionsEnd) {
-			// Help text
-			if ((argv[i] == "-h") || (argv[i] == "--help")) {
-				printUsage(stdout, argv[0]);
-				returnValue = 0;
-
-				return false;
-			}
-
-			if (argv[i] == "--version") {
-				Version::printVersion();
-				returnValue = 0;
-
-				return false;
-			}
-
-			if        (argv[i] == "--cp1252") {
-				// Set the GFF4 string encoding to CP1252
-
-				isOption = true;
-				encoding = Common::kEncodingCP1252;
-
-			} else if (argv[i] == "--nwnpremium") {
-
-				isOption   = true;
-				nwnPremium = true;
-
-			} else if (argv[i] == "--nwn") {
-				isOption = true;
-				game     = Aurora::kGameIDNWN;
-			} else if (argv[i] == "--nwn2") {
-				isOption = true;
-				game     = Aurora::kGameIDNWN2;
-			} else if (argv[i] == "--kotor") {
-				isOption = true;
-				game     = Aurora::kGameIDKotOR;
-			} else if (argv[i] == "--kotor2") {
-				isOption = true;
-				game     = Aurora::kGameIDKotOR2;
-			} else if (argv[i] == "--jade") {
-				isOption = true;
-				game     = Aurora::kGameIDJade;
-			} else if (argv[i] == "--witcher") {
-				isOption = true;
-				game     = Aurora::kGameIDWitcher;
-			} else if (argv[i] == "--dragonage") {
-				isOption = true;
-				game     = Aurora::kGameIDDragonAge;
-			} else if (argv[i] == "--dragonage2") {
-				isOption = true;
-				game     = Aurora::kGameIDDragonAge2;
-
-			} else if (argv[i] == "--encoding") {
-				isOption = true;
-
-				if ((i++ == (argv.size() - 1)) || !parseEncodingOverride(argv[i], encOverrides)) {
-					printUsage(stdout, argv[0]);
-					returnValue = 1;
-
-					return false;
-				}
-
-			} else if (argv[i].beginsWith("-") || argv[i].beginsWith("--")) {
-			  // An options, but we already checked for all known ones
-
-				printUsage(stderr, argv[0]);
-				returnValue = 1;
-
-				return false;
-			}
-		}
-
-		// Was this a valid option? If so, don't try to use it as a file
-		if (isOption)
-			continue;
-
-		// This is a file to use
-		args.push_back(argv[i]);
-	}
-
-	if ((args.size() < 1) || (args.size() > 2)) {
-		printUsage(stderr, argv[0]);
-		returnValue = 1;
-
-		return false;
-	}
-
-	inFile = args[0];
-
-	if (args.size() > 1)
-		outFile = args[1];
-
-	return true;
+	return parser.process(argv);
 }
 
-void printUsage(FILE *stream, const Common::UString &name) {
-	std::fprintf(stream, "BioWare GFF to XML converter\n\n");
-	std::fprintf(stream, "Usage: %s [<options>] <input file> [<output file>]\n", name.c_str());
-	std::fprintf(stream, "  -h      --help              This help text\n");
-	std::fprintf(stream, "          --version           Display version information\n");
-	std::fprintf(stream, "          --cp1252            Read GFF4 strings as Windows CP-1252\n");
-	std::fprintf(stream, "          --nwnpremium        This is a broken GFF from a Neverwinter\n");
-	std::fprintf(stream, "                              Nights premium module\n\n");
-	std::fprintf(stream, "          --nwn               Use Neverwinter Nights encodings\n");
-	std::fprintf(stream, "          --nwn2              Use Neverwinter Nights 2 encodings\n");
-	std::fprintf(stream, "          --kotor             Use Knights of the Old Republic encodings\n");
-	std::fprintf(stream, "          --kotor2            Use Knights of the Old Republic II encodings\n");
-	std::fprintf(stream, "          --jade              Use Jade Empire encodings\n");
-	std::fprintf(stream, "          --witcher           Use The Witcher encodings\n");
-	std::fprintf(stream, "          --dragonage         Use Dragon Age encodings\n");
-	std::fprintf(stream, "          --dragonage2        Use Dragon Age II encodings\n\n");
-	std::fprintf(stream, "          --encoding <str>    Override an encoding\n\n");
-	std::fprintf(stream, "If no output file is given, the output is written to stdout.\n\n");
-	std::fprintf(stream, "Depending on the game, LocStrings in GFF files might be encoded in various\n");
-	std::fprintf(stream, "ways and there's no way to autodetect how. If a game is specified, the\n");
-	std::fprintf(stream, "encoding tables for this game are used. Otherwise, gff2xml tries some\n");
-	std::fprintf(stream, "heuristics that might fail for certain strings.\n\n");
-	std::fprintf(stream, "Additionally, the --encoding parameter can be used to override the encoding\n");
-	std::fprintf(stream, "for a specific language ID. The string has to be of the form n=encoding,\n");
-	std::fprintf(stream, "for example 0=cp-1252 to override the encoding of the (ungendered) language\n");
-	std::fprintf(stream, "ID 0 to be Windows codepage 1252. To override several encodings, specify\n");
-	std::fprintf(stream, "the --encoding parameter multiple times.\n");
-}
 
 void dumpGFF(const Common::UString &inFile, const Common::UString &outFile,
              Common::Encoding encoding, bool nwnPremium) {
