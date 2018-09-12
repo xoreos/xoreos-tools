20fb1163322d6564acbf18d75db4e1989fd894d7
patrick <nostritius@googlemail.com>
2018-09-12 12:19:26 +0200

    AURORA: Add the SACFile class

--- src/aurora/rules.mk
+++ src/aurora/rules.mk
@@ -54,6 +54,7 @@ src_aurora_libaurora_la_SOURCES += \
     src/aurora/nitrofile.h \
     src/aurora/nsbtxfile.h \
     src/aurora/erfwriter.h \
+    src/aurora/sacfile.h \
     $(EMPTY)
 
 src_aurora_libaurora_la_SOURCES += \
@@ -84,4 +85,5 @@ src_aurora_libaurora_la_SOURCES += \
     src/aurora/nitrofile.cpp \
     src/aurora/nsbtxfile.cpp \
     src/aurora/erfwriter.cpp \
+    src/aurora/sacfile.cpp \
     $(EMPTY)
