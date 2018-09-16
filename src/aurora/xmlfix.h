80b25c8b847162df1b90d1a27b13ed86e319d7e1
Bob Hall <43081644+rjshae@users.noreply.github.com>
2018-09-16 14:00:59 -0700

    NWN2: Fix typo in a return type and add header

--- src/aurora/xmlfix.h
+++ src/aurora/xmlfix.h
@@ -1,3 +1,30 @@
+/* xoreos-tools - Tools to help with xoreos development
+ *
+ * xoreos-tools is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos-tools is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos-tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Fix broken, non-standard XML files.
+ */
+
+#ifndef AURORA_XMLFIX_H
+#define AURORA_XMLFIX_H
+
 #include <iostream>
 #include <string>
 
@@ -23,7 +50,7 @@ private:
 	void replaceAll(Common::UString& str, const Common::UString& from, const Common::UString& to);
 	int countOccurances(Common::UString line, char find);
 	Common::UString fixCopyright(Common::UString line);
-	Common::UStringg fixXMLTag(Common::UString line);
+	Common::UString fixXMLTag(Common::UString line);
 	Common::UString doubleDashFix(Common::UString line);
 	Common::UString tripleQuoteFix(Common::UString line);
 	void countComments(std::string line);
@@ -39,4 +66,4 @@ private:
 
 } // End of namespace Aurora
 
-
+#endif // AURORA_XMLFIX_H
