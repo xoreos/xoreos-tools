a52f0f5b70984a40950579bfbe59ee3c0367c448
Sven Hesse <drmccoy@drmccoy.de>
2025-01-02 15:14:43 +0100

    COMMON: Remove <> from templated class constructor/destructor

--- src/common/singleton.h
+++ src/common/singleton.h
@@ -60,8 +60,8 @@ namespace Common {
 template<class T>
 class Singleton : boost::noncopyable {
 private:
-	Singleton<T>(const Singleton<T> &);
-	Singleton<T> &operator=(const Singleton<T> &);
+	Singleton(const Singleton<T> &);
+	Singleton &operator=(const Singleton<T> &);
 
 	static T *_singleton;
 
@@ -105,8 +105,8 @@ public:
 		T::destroyInstance();
 	}
 protected:
-	Singleton<T>() { }
-	virtual ~Singleton<T>() { }
+	Singleton() { }
+	virtual ~Singleton() { }
 
 	typedef T SingletonBaseType;
 };
